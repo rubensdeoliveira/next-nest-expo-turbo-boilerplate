6b134f6df05d373e3ab25afdb2d3fb62b38f1b1c
Rubens Junior <rubensjuniordev@gmail.com>
2024-12-29 10:02:05 -0300

    feat: fix package typescript

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-// import { env } from '@next-nest-react-native-turbo-boilerplate/env'
+import { env } from '@next-nest-react-native-turbo-boilerplate/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+  await app.listen(env.PORT)
 }
 bootstrap()
